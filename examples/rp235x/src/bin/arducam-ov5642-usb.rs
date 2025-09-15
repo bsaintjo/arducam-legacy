5f3ae4b12c95e4a399aebee74a9646a628ad6688
Brandon Saint-John <saint-john@lbl.gov>
2025-09-15 02:07:18 -0700

    attempt usb version

--- examples/rp235x/src/bin/arducam-ov5642-usb.rs
+++ examples/rp235x/src/bin/arducam-ov5642-usb.rs
@@ -8,12 +8,17 @@ use arducam_legacy::{
 use defmt::*;
 use embassy_executor::Spawner;
 use embassy_rp::{
+    bind_interrupts,
     gpio::{Level, Output},
     i2c::I2c,
+    peripherals::USB,
     spi::Spi,
+    usb::{Driver, InterruptHandler},
 };
 use embassy_time::{Delay, Timer};
+use embassy_usb::{class::cdc_acm::{CdcAcmClass, State}, UsbDevice};
 use embedded_hal_bus::spi::ExclusiveDevice;
+use static_cell::StaticCell;
 // use serde::Serialize;
 // use serde_json_core::{heapless::String, to_string};
 use {defmt_rtt as _, panic_probe as _};
@@ -24,9 +29,58 @@ use {defmt_rtt as _, panic_probe as _};
 //     bytes: &'a [u8]
 // }
 
+bind_interrupts!(struct Irqs {
+    USBCTRL_IRQ => InterruptHandler<USB>;
+});
+
+type UDevice = UsbDevice<'static, Driver<'static, USB>>;
+
+#[embassy_executor::task]
+async fn usb_task(mut usb: UDevice) -> ! {
+    usb.run().await
+}
+
 #[embassy_executor::main]
-async fn main(_spawner: Spawner) {
+async fn main(spawner: Spawner) {
     let p = embassy_rp::init(Default::default());
+    let driver = Driver::new(p.USB, Irqs);
+
+    let config = {
+        let mut config = embassy_usb::Config::new(0xc0de, 0xcafe);
+        config.manufacturer = Some("Embassy");
+        config.product = Some("USB-serial example");
+        config.serial_number = Some("12345678");
+        config.max_power = 100;
+        config.max_packet_size_0 = 64;
+        config
+    };
+
+    let mut builder = {
+        static CONFIG_DESCRIPTOR: StaticCell<[u8; 256]> = StaticCell::new();
+        static BOS_DESCRIPTOR: StaticCell<[u8; 256]> = StaticCell::new();
+        static CONTROL_BUF: StaticCell<[u8; 64]> = StaticCell::new();
+
+        let builder = embassy_usb::Builder::new(
+            driver,
+            config,
+            CONFIG_DESCRIPTOR.init([0; 256]),
+            BOS_DESCRIPTOR.init([0; 256]),
+            &mut [], // no msos descriptors
+            CONTROL_BUF.init([0; 64]),
+        );
+        builder
+    };
+
+    let mut class = {
+        static STATE: StaticCell<State> = StaticCell::new();
+        let state = STATE.init(State::new());
+        CdcAcmClass::new(&mut builder, state, 64)
+    };
+
+    let usb = builder.build();
+    spawner.spawn(usb_task(usb));
+    class.wait_connection().await;
+
     let scl = p.PIN_17;
     let sda = p.PIN_16;
     let i2c = I2c::new_blocking(p.I2C0, scl, sda, Default::default());
@@ -59,7 +113,9 @@ async fn main(_spawner: Spawner) {
     info!("Initialized!");
 
     arducam.vsync_mask(&mut delay).unwrap();
-    arducam.set_jpeg_size(Resolution::Res320x240, &mut delay).unwrap();
+    arducam
+        .set_jpeg_size(Resolution::Res320x240, &mut delay)
+        .unwrap();
     // Timer::after_millis(1000).await;
     arducam.clear_fifo_flag(&mut delay).unwrap();
     arducam.frames(&mut delay).unwrap();
@@ -99,6 +155,7 @@ async fn main(_spawner: Spawner) {
             "End marker: {:?}",
             image.windows(2).position(|pair| pair == [0xff, 0xd9])
         );
-        arducam.clear_fifo_flag(&mut delay);
+        class.write_packet(&image[..fifo_length as usize]).await;
+        arducam.clear_fifo_flag(&mut delay).unwrap();
     }
 }
